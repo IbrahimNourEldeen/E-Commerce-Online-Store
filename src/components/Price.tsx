715e3f5af5bd36cf6a6391682793bce6f67d6a91
IbrahimNourEldeen <ibrahimnoureldeen11@gmail.com>
2025-06-15 18:22:03 +0300

    handle filter by price in home sidebar

--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -1,4 +1,55 @@
+import type React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  AddFilteredProducts,
+  ClearFilterdProducts,
+  type Product,
+} from "../features/products/productSlice";
+import type { RootState } from "../store/Store";
+
 const Price = () => {
+  const dispatch = useDispatch();
+  const { filteredProducts, products } = useSelector(
+    (state: RootState) => state.products
+  );
+
+  const handlePrice = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    let filtered: Product[] = [];
+
+    switch (value) {
+      case "all":
+        dispatch(ClearFilterdProducts());
+        return;
+
+      case "Under EGP 100":
+        filtered =
+          products?.filter((product) => product.price<=100) ||
+          [];
+        break;
+      case "Under EGP 400":
+        filtered =
+          products?.filter((product) => product.price<=400) ||
+          [];
+        break;
+      case "Under EGP 800":
+        filtered =
+          products?.filter((product) => product.price<=800) ||
+          [];
+        break;
+      case "Under EGP 1600":
+        filtered =
+          products?.filter((product) => product.price<=1600) ||
+          [];
+        break;
+      case "EGP 1,600 & above":
+        filtered =  products || []
+        break;
+      default:
+        break;
+    }
+    dispatch(AddFilteredProducts(filtered.sort((a,b)=>b.price-a.price)||null));
+  };
   return (
     <>
       <h4 className="font-bold">Price</h4>
@@ -10,7 +61,7 @@ const Price = () => {
             className="me-2"
             name="price"
             value="all"
-            //   onChange={handleBrand}
+            onChange={handlePrice}
           />
           <label htmlFor="allrevs">All</label>
         </div>
@@ -20,11 +71,11 @@ const Price = () => {
             type="radio"
             className="me-2"
             name="price"
-            value="Under EGP 400"
-            //   onChange={handleBrand}
+            value="Under EGP 100"
+            onChange={handlePrice}
           />
           <label htmlFor="range1" className="">
-            Under EGP 400
+            Under EGP 100
           </label>
         </div>
         <div className="flex">
@@ -33,11 +84,11 @@ const Price = () => {
             type="radio"
             className="me-2"
             name="price"
-            value="Under EGP 800"
-            //   onChange={handleBrand}
+            value="Under EGP 400"
+            onChange={handlePrice}
           />
           <label htmlFor="range2" className="">
-            Under EGP 800
+            Under EGP 400
           </label>
         </div>
         <div className="flex">
@@ -46,11 +97,11 @@ const Price = () => {
             type="radio"
             className="me-2"
             name="price"
-            value="Under EGP 1200"
-            //   onChange={handleBrand}
+            value="Under EGP 800"
+            onChange={handlePrice}
           />
           <label htmlFor="range3" className="">
-            Under EGP 1200
+            Under EGP 800
           </label>
         </div>
         <div className="flex">
@@ -60,7 +111,7 @@ const Price = () => {
             className="me-2"
             name="price"
             value="Under EGP 1600"
-            //   onChange={handleBrand}
+            onChange={handlePrice}
           />
           <label htmlFor="range4" className="">
             Under EGP 1600
@@ -73,7 +124,7 @@ const Price = () => {
             className="me-2"
             name="price"
             value="EGP 1,600 & above"
-            //   onChange={handleBrand}
+            onChange={handlePrice}
           />
           <label htmlFor="range5" className="">
             EGP 1,600 & above
