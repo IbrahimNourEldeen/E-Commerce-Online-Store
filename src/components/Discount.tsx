300367abf2f09b0db2014665e0b7a9ce96373b24
IbrahimNourEldeen <ibrahimnoureldeen11@gmail.com>
2025-06-16 21:38:41 +0300

    remove frequency by using map in discount file

--- src/components/Discount.tsx
+++ src/components/Discount.tsx
@@ -1,12 +1,14 @@
 import { useDispatch, useSelector } from "react-redux";
-import { AddFilteredProducts, ClearFilterdProducts, type Product } from "../features/products/productSlice";
+import {
+  AddFilteredProducts,
+  ClearFilterdProducts,
+  type Product,
+} from "../features/products/productSlice";
 import type { RootState } from "../store/Store";
 
 const Discount = () => {
-   const dispatch = useDispatch();
-  const { products } = useSelector(
-    (state: RootState) => state.products
-  );
+  const dispatch = useDispatch();
+  const { products } = useSelector((state: RootState) => state.products);
 
   const handleDiscount = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
@@ -19,96 +21,73 @@ const Discount = () => {
 
       case "10% off or more":
         filtered =
-          products?.filter((product) => product.discountPercentage>=10) ||
-          [];
+          products?.filter((product) => product.discountPercentage >= 10) || [];
         break;
       case "25% off or more":
         filtered =
-          products?.filter((product) => product.discountPercentage>=25) ||
-          [];
+          products?.filter((product) => product.discountPercentage >= 25) || [];
         break;
       case "50% off or more":
         filtered =
-          products?.filter((product) => product.discountPercentage>=50) ||
-          [];
+          products?.filter((product) => product.discountPercentage >= 50) || [];
         break;
       case "70% off or more":
         filtered =
-          products?.filter((product) => product.discountPercentage>=70) ||
-          [];
+          products?.filter((product) => product.discountPercentage >= 70) || [];
         break;
       default:
         break;
     }
-    dispatch(AddFilteredProducts(filtered||null));
+    dispatch(AddFilteredProducts(filtered || null));
   };
+
+  const discountFields = [
+    {
+      id: "alldiscounts",
+      value: "all",
+      label: "All",
+    },
+    {
+      id: "num1",
+      value: "10% off or more",
+      label: "10% off or more",
+    },
+    {
+      id: "num2",
+      value: "25% off or more",
+      label: "25% off or more",
+    },
+    {
+      id: "num3",
+      value: "50% off or more",
+      label: "50% off or more",
+    },
+    {
+      id: "num4",
+      value: "70% off or more",
+      label: "70% off or more",
+    }
+  ];
   return (
     <>
       <h4 className="font-bold">Discount</h4>
       <div className="">
-        <div className="">
-          <input
-            id="alldiscounts"
-            type="radio"
-            className="me-2"
-            name="discount"
-            value="all"
+        {discountFields.map((field, index) => (
+          <div className="flex">
+            <input
+              key={index}
+              id={field.id}
+              type="radio"
+              className="me-2"
+              name="discount"
+              value={field.value}
               onChange={handleDiscount}
-          />
-          <label htmlFor="allrevs">All</label>
-        </div>
-        <div className="flex">
-          <input
-            id="num1"
-            type="radio"
-            className="me-2"
-            name="discount"
-            value="10% off or more"
-              onChange={handleDiscount}
-          />
-          <label htmlFor="num1" className="">
-            10% off or more
-          </label>
-        </div>
-        <div className="flex">
-          <input
-            id="num2"
-            type="radio"
-            className="me-2"
-            name="discount"
-            value="25% off or more"
-              onChange={handleDiscount}
-          />
-          <label htmlFor="num2" className="">
-            25% off or more
-          </label>
-        </div>
-        <div className="flex">
-          <input
-            id="num3"
-            type="radio"
-            className="me-2"
-            name="discount"
-            value="50% off or more"
-              onChange={handleDiscount}
-          />
-          <label htmlFor="num3" className="">
-            50% off or more
-          </label>
-        </div>
-        <div className="flex">
-          <input
-            id="num4"
-            type="radio"
-            className="me-2"
-            name="discount"
-            value="70% off or more"
-              onChange={handleDiscount}
-          />
-          <label htmlFor="num4" className="">
-            70% off or more
-          </label>
-        </div>
+            />
+            <label htmlFor={field.id} className="">
+              {field.label}
+            </label>
+          </div>
+        ))}
       </div>
     </>
   );
